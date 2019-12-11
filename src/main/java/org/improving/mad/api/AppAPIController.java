c7d92e166348a888c355249e5a4134c5351c521c
Ethan Miller <Ethan.Miller@improving.com>
2019-12-10 20:43:32 -0600

    relace entity manager with crud repository

--- src/main/java/org/improving/mad/api/AppAPIController.java
+++ src/main/java/org/improving/mad/api/AppAPIController.java
@@ -2,6 +2,7 @@ package org.improving.mad.api;
 
 import org.improving.mad.MovieRepository;
 import org.improving.mad.entity.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,25 +12,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class AppAPIController {
 
+    @Autowired
     private MovieRepository movieRepository;
-
-    public AppAPIController(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
 
     @GetMapping("/movies")
     public List<Movie> movies() {
-        return movieRepository.getMovies();
+        return movieRepository.findAll();
     }
 
     @GetMapping("/movies/{movieId}")
     public Movie movies(@PathVariable int movieId) {
-        return movieRepository.getMovie(movieId);
+        return movieRepository.findById(movieId);
     }
 
-    @PostMapping("/movies/form")
-    public void movies(@RequestParam String title, @RequestParam int year, @RequestParam int rating,
-                       @RequestParam int runTime) {
-        movieRepository.addMovie(title, year, rating, runTime);
-    }
 }
